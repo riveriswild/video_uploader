fdf32b02505082401cad1d82902384d557ead2b4
River <riveriswild@icloud.com>
2022-12-28 04:09:48 +0300

    Add to models

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Optional
 
 import ormar
 from db import metadata, database
@@ -27,4 +28,5 @@ class Video(ormar.Model):
     description: str = ormar.String(max_length=500)
     file: str = ormar.String(max_length=1000)
     create_at: datetime.datetime = ormar.DateTime(default=datetime.datetime.now)
+    user: Optional[User] = ormar.ForeignKey(User)
 
