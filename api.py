be639d6c52ff437e6073b3e923c3ae78071727b3
River <riveriswild@icloud.com>
2022-12-28 06:32:06 +0300

    Add api views

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ from urllib import response
 from fastapi import APIRouter, UploadFile, File, Form, Request
 from fastapi.responses import JSONResponse
 
-from schemas import UploadVideo, GetVideo, User, Message
-from models import Video
+from schemas import UploadVideo, GetVideo, Message
+from models import Video, User
 
 video_router = APIRouter()
 
@@ -16,34 +16,31 @@ async def root(title: str = Form(...), description: str = Form(...), file: Uploa
     info = UploadVideo(title=title, description=description)
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
-    return {"file_name": file.filename, 'info': info}
+    user = await User.objects.first()
+    return await Video.objects.create(file = file.filename, user=user, **info.dict())
 
 
-@video_router.post("/img")
-async def upload_image(files: List[UploadFile] = File(...)):
-    for img in files:
-        with open(f'{img.filename}', 'wb') as buffer:
-            shutil.copyfileobj(img.file, buffer)
-    return {"file_name": img.filename}
+# @video_router.post("/img")
+# async def upload_image(files: List[UploadFile] = File(...)):
+#     for img in files:
+#         with open(f'{img.filename}', 'wb') as buffer:
+#             shutil.copyfileobj(img.file, buffer)
+#     return {"file_name": img.filename}
 
 
 # @video_router.post("/info")
 # async def info_set(info: UploadVideo):
 #     return info
 
-@video_router.post("/video")
-async def create_video(video: Video):
-    await video.save()
-    return video
+# @video_router.post("/video")
+# async def create_video(video: Video):
+#     await video.save()
+#     return video
 
-@video_router.get("/video", response_model=GetVideo, responses={404: {"model": Message}})
-async def get_video():
-    # user = {'id': 25, 'name': 'Doe'}
-    user = User(**{'id': 25, 'name': 'Doe'}) # 2nd option
-    # video = {'title': 'Test', 'description': 'Description'}
-    video = UploadVideo(**{'title': 'Test', 'description': 'Description'})  # 2nd option
-    info = GetVideo(user=user, video=video)
-    return JSONResponse(status_code=200, content=info.dict())
+# @video_router.get("/video/{video_pk}", response_model=Video, responses={404: {"model": Message}})
+@video_router.get("/video/{video_pk}", response_model=GetVideo, responses={404: {"model": Message}})
+async def get_video(video_pk: int):
+    return await Video.objects.select_related('user').get(pk=video_pk)
 
 @video_router.get("/test")
 async def get_test(req: Request):
