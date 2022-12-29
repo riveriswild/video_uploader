838b24620d43fa179250ee271fba97a9e4d3037b
River <riveriswild@icloud.com>
2022-12-29 04:09:55 +0300

    Video streaming functionality:

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ from starlette.templating import Jinja2Templates
 
 from models import User, Video
 from schemas import GetListVideo, GetVideo, Message, UploadVideo
-from services import save_video
+from services import open_file, save_video
 
 video_router = APIRouter()
 templates = Jinja2Templates(directory='templates')
@@ -30,13 +30,13 @@ async def create_video(
     return await save_video(user, file, title, description, background_tasks)
 
 
-@video_router.get("/video/{video_pk}")
-async def get_video(video_pk: int):
-    file = await Video.objects.select_related('user').get(pk=video_pk)
-    # video_path = os.path.join('media', file.dict().get('file'))
-    # video_path = os.path.join('media', file.file)
-    file_like = open(file.file, mode='rb')
-    return StreamingResponse(file_like, media_type="video/mp4")
+# @video_router.get("/video/{video_pk}")
+# async def get_video(video_pk: int):
+#     file = await Video.objects.select_related('user').get(pk=video_pk)
+#     # video_path = os.path.join('media', file.dict().get('file'))
+#     # video_path = os.path.join('media', file.file)
+#     file_like = open(file.file, mode='rb')
+#     return StreamingResponse(file_like, media_type="video/mp4")
 
 
 @video_router.get('/user/{user_pk}', response_model=List[GetListVideo])
@@ -48,3 +48,18 @@ async def get_list_video(user_pk: int):
 @video_router.get("index/{video_pk}", response_class=HTMLResponse)
 async def get_video(request: Request, video_pk: int):
     return templates.TemplateResponse("index.html",{"request": request, "path": video_pk})
+
+@video_router.get("/video/{video_pk}")
+async def get_streaming_video(request: Request, video_pk: int) -> StreamingResponse:
+    file, status_code, content_length, headers = await open_file(request, video_pk)
+    response = StreamingResponse(
+        file,
+        media_type="video/mp4",
+        status_code=status_code,
+    )
+    response.headers.update({
+        'Accept-Ranges':'bytes',
+        'Content-Length': str(content_length),
+        **headers,
+    })
+    return response
--- services.py
+++ services.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 import aiofiles
 import shutil
-from fastapi import UploadFile, HTTPException
+from fastapi import Request, UploadFile, HTTPException
 from uuid import uuid4
+from typing import IO, Generator
 
 from fastapi import BackgroundTasks
+import ormar
 
 from models import Video
 from schemas import UploadVideo
@@ -34,3 +37,53 @@ async def write_video(file_name: str, file: UploadFile):
     async with aiofiles.open(file_name, 'wb') as buffer:
         data = await file.read()
         await buffer.write(data)
+        
+def ranged(
+        file: IO[bytes],
+        start: int = 0,
+        end: int = None,
+        block_size: int = 8192,
+) -> Generator[bytes, None, None]:
+    consumed = 0
+
+    file.seek(start)
+    while True:
+        data_length = min(block_size, end - start - consumed) if end else block_size
+        if data_length <= 0:
+            break
+        data = file.read(data_length)
+        if not data:
+            break
+        consumed += data_length
+        yield data
+
+    if hasattr(file, 'close'):
+        file.close()
+        
+
+async def open_file(request: Request, video_pk:int):
+    try:
+        file = await Video.objects.get(pk=video_pk)
+    except ormar.exceptions.NoMatch:
+        raise HTTPException(status_code=404, detail="Not Found")
+    path = Path(file.dict().get('file'))
+    file = path.open('rb')
+    file_size = path.stat().st_size
+    
+    content_length = file_size
+    status_code = 200
+    headers = {}
+    content_range = request.headers.get('range')
+    
+    if content_range is not None:
+        content_range = content_range.strip().lower()
+        content_ranges = content_range.split('=')[-1]
+        range_start, range_end, *_ = map(str.strip, (content_ranges+ '-').split('-'))
+        range_start = max(0, int(range_start)) if range_start else 0
+        range_end = min(file_size - 1, range_end) if range_end else file_size - 1
+        content_length = (range_end - range_start) + 1
+        file = ranged(file, start=range_start, end=range_end+1)
+        status_code = 206
+        headers['Content-Range'] = f'bytes {range_start}-{range_end}/{file_size}'
+    return file, status_code,content_length, headers
+    
